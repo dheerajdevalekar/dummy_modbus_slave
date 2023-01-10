744f614aa7d37c350c3cd008c1cf963b97f13f5a
Pratik Anekar <pratik.a@smart-iam.com>
2023-01-10 12:51:26 +0530

    updated code

--- create_csv.py
+++ create_csv.py
@@ -2,88 +2,31 @@ import random
 import csv
 from os import getcwd
 from loguru import logger
-holding_value = []
-coil_value = []
-input_value = []
-temp_header = ["temp_key", "temp_value"]
+from time import sleep
 
 
-def creating_csv_file(file):
+def make_register_dict(register_type: str, start_reg_addr=1, end_reg_addr=65537, start_val=0, end_val=65535):
+    register_key_val = {}
+    file_ = None
+    path = f"{getcwd()}/modbus/{register_type}.csv"
     try:
-        write_csv_file = open(f'{getcwd()}/modbus/{file}', "w+")
-        writer = csv.writer(write_csv_file)
-        count = 0
-        logger.debug(f"file {file} is writing started please wait....")
-        if file == "holding.csv":
-            for data in holding_value:
-                if count == 0:
-                    header = data.keys()
-                    writer.writerow(header)
-                    count += 1
-                writer.writerow(data.values())
-        elif file == "input.csv":
-            for data in input_value:
-                if count == 0:
-                    header = data.keys()
-                    writer.writerow(header)
-                    count += 1
-                writer.writerow(data.values())
-        elif file == "coil.csv":
-            for data in coil_value:
-                if count == 0:
-                    header = data.keys()
-                    writer.writerow(header)
-                    count += 1
-                writer.writerow(data.values())
+        for i in range(start_reg_addr, end_reg_addr):
+            random_val = random.randint(start_val, end_val)
+            register_key_val.update({i: random_val})
+        file_ = open(path, "w+")
+        writer = csv.writer(file_)
+        for j in register_key_val:
+            writer.writerow([j, register_key_val.get(j)])
+        logger.debug(f"Done file creation of {path}")
     except Exception as e:
-        logger.error(str(e))
+        logger.error(f"Error on {path}: {e}")
+    finally:
+        if file_ is not None:
+            file_.close()
+        sleep(2)
 
 
-def creating_json_for_hold_input(i, j, k, file):
-    try:
-        for key_ in range(j, (k+1)):
-            data = {}
-            value_ = random.randint(i, (k-1))
-            data.update({temp_header[0]: key_})
-            data.update({temp_header[1]: value_})
-            if file == "holding.csv":
-                holding_value.append(data)
-            elif file == "input.csv":
-                input_value.append(data)
-    except Exception as e:
-        logger.error(str(e))
-
-
-def creating_json_for_coil(i, j, k, file):
-    try:
-        for key_ in range(j, (k+1)):
-            data = {}
-            value_ = random.randint(i, j)
-            data.update({temp_header[0]: key_})
-            data.update({temp_header[1]: value_})
-            coil_value.append(data)
-    except Exception as e:
-        logger.error(str(e))
-
-
-if __name__ == "__main__":
-    i = 0
-    j = 1
-    k = 65536
-    temp_filename = ["input.csv", "holding.csv", "coil.csv"]
-    try:
-        for file in temp_filename:
-            if file == "holding.csv":
-                creating_json_for_hold_input(i, j, k, file)
-                creating_csv_file(file)
-                logger.info(f"file is successfully created in {getcwd()}/modbus/{file}")
-            elif file == "coil.csv":
-                creating_json_for_coil(i, j, k, file)
-                creating_csv_file(file)
-                logger.info(f"file is successfully created in {getcwd()}/modbus/{file}")
-            elif file == "input.csv":
-                creating_json_for_hold_input(i, j, k, file)
-                creating_csv_file(file)
-                logger.info(f"file is successfully created in {getcwd()}/modbus/{file}")
-    except Exception as e:
-        logger.error(str(e))
+if __name__ == '__main__':
+    make_register_dict(register_type="holding")
+    make_register_dict(register_type="input")
+    make_register_dict(register_type="coils", start_val=0, end_val=1)
